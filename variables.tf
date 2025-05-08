ecfb05c3b151de8de27aa205908e1c4f57dd8f59
franco0306 <francoalessandro0306@gmail.com>
2025-05-07 23:03:45 -0500

    solucion de error

--- variables.tf
+++ variables.tf
@@ -7,3 +7,10 @@ variable "environment" {
     description = "Entorno de despliegue (dev, staging, prod)"
     type        = string
 }
+
+
+variable "bucket_name" {
+  description = "Nombre del bucket del sitio estático"
+  type        = string
+  default     = "evento-static-site-2025-demo"
+}
